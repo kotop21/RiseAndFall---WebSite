ca5e0da81dfe4841d776fb38610b8909d976fc22
Victor <72082776+kotop21@users.noreply.github.com>
2026-04-14 19:58:05 +0300

    Rename project v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: '[https://kotop21.github.io](https://kotop21.github.io)',
+  site: 'https://kotop21.github.io',
   base: '/RiseAndFall',
   trailingSlash: 'always',
   build: {
